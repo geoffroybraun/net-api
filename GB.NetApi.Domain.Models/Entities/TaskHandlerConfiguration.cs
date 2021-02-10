db51ca5f6b94ced449655952d70f76ccbe18e760
Geoffroy BRAUN <geoffroy.braun@gmail.com>
2021-02-10 14:54:39 +0100

    Add task handler using "Polly" NuGet package

---
+++ GB.NetApi.Domain.Models/Entities/TaskHandlerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace GB.NetApi.Domain.Models.Entities
+{
+    /// <summary>
+    /// Represents a configuration used by a <see cref="Interfaces.Libraries.ITaskHandler"/> implementation
+    /// </summary>
+    public sealed record TaskHandlerConfiguration
+    {
+        public int MaxRetriesCount { get; init; }
+
+        public int MaxExceptionsCount { get; init; }
+
+        public int TimeBetweenBreaksInMilliseconds { get; init; }
+
+        public int TimeoutInMilliseconds { get; init; }
+
+        public int MaxParalellizationCount { get; init; }
+
+        public int MaxQueuedActionsCount { get; init; }
+    }
+}
