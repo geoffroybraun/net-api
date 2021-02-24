b14436801b88a955fcb34124c7ce5b23db586761
Geoffroy BRAUN <geoffroy.braun@gmail.com>
2021-02-24 09:21:58 +0100

    Add user name to authentication process to allow multiple accounts with the same email address

--- GB.NetApi.Application.Services/Queries/AuthenticateUsers/GetSingleAuthenticateUserQuery.cs
+++ GB.NetApi.Application.Services/Queries/AuthenticateUsers/GetSingleAuthenticateUserQuery.cs
@@ -11,5 +11,7 @@ namespace GB.NetApi.Application.Services.Queries.AuthenticateUsers
     public sealed record GetSingleAuthenticateUserQuery : IQuery<AuthenticateUserDto>
     {
         public string UserEmail { get; init; }
+
+        public string UserName { get; init; }
     }
 }
