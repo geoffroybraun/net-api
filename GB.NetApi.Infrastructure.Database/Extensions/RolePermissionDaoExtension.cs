d9f9fcf31e29720fd1b0d6b9a39678c3d9d9ccdf
Geoffroy BRAUN <geoffroy.braun@gmail.com>
2021-02-24 08:45:43 +0100

    Reduce extension methods in "Infrastructure.Database" project to increase readibility

--- GB.NetApi.Infrastructure.Database/Extensions/RolePermissionDaoExtension.cs
+++ GB.NetApi.Infrastructure.Database/Extensions/RolePermissionDaoExtension.cs
@@ -1,4 +1,7 @@
-﻿using GB.NetApi.Infrastructure.Database.DAOs.Identity;
+﻿using GB.NetApi.Domain.Services.Extensions;
+using GB.NetApi.Infrastructure.Database.DAOs.Identity;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GB.NetApi.Infrastructure.Database.Extensions
 {
@@ -7,6 +10,16 @@ namespace GB.NetApi.Infrastructure.Database.Extensions
     /// </summary>
     public static class RolePermissionDaoExtension
     {
+        /// <summary>
+        /// Retrieve the extended <see cref="IEnumerable{RolePermissionDao}"/> implementation as permission names
+        /// </summary>
+        /// <param name="rolePermissions">The extended <see cref="IEnumerable{RolePermissionDao}"/> implementation to convert</param>
+        /// <returns>The built permission names</returns>
+        public static IEnumerable<string> GetPermissionNames(this IEnumerable<RolePermissionDao> rolePermissions)
+        {
+            return rolePermissions.IsNotNullNorEmpty() ? rolePermissions.Select(GetPermissionName) : default;
+        }
+
         /// <summary>
         /// Retrieve the extended <see cref="RolePermissionDao"/> as a permission name
         /// </summary>
