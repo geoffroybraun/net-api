75444cb59fda90c51147b22b29a4add34f48bea0
Geoffroy BRAUN <geoffroy.braun@gmail.com>
2021-02-24 21:44:07 +0100

    Revert "Reduce extension methods in "Infrastructure.Database" project to increase readibility"
    This reverts commit d9f9fcf31e29720fd1b0d6b9a39678c3d9d9ccdf.

--- GB.NetApi.Infrastructure.Database/Extensions/UserRoleDaoExtension.cs
+++ GB.NetApi.Infrastructure.Database/Extensions/UserRoleDaoExtension.cs
@@ -1,7 +1,5 @@
-﻿using GB.NetApi.Domain.Services.Extensions;
-using GB.NetApi.Infrastructure.Database.DAOs.Identity;
+﻿using GB.NetApi.Infrastructure.Database.DAOs.Identity;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 
 namespace GB.NetApi.Infrastructure.Database.Extensions
@@ -12,16 +10,6 @@ namespace GB.NetApi.Infrastructure.Database.Extensions
     public static class UserRoleDaoExtension
     {
         /// <summary>
-        /// Retrieve the extended <see cref="IEnumerable{UserRoleDao}"/> implementation as permission names
-        /// </summary>
-        /// <param name="userRoles">The extended <see cref="IEnumerable{UserRoleDao}"/> implementation to convert</param>
-        /// <returns>The built permission names</returns>
-        public static IEnumerable<string> GetPermissionNames(this IEnumerable<UserRoleDao> userRoles)
-        {
-            return userRoles.IsNotNullNorEmpty() ? userRoles.SelectMany(GetPermissionNames) : default;
-        }
-
-        /// <summary>
         /// Retrieve the extended <see cref="UserRoleDao"/> as permission names
         /// </summary>
         /// <param name="userRole">The extended <see cref="UserRoleDao"/></param>
@@ -29,16 +17,6 @@ namespace GB.NetApi.Infrastructure.Database.Extensions
         public static IEnumerable<string> GetPermissionNames(this UserRoleDao userRole)
         {
             return userRole is not null ? userRole.Role.GetPermissionNames() : default;
-        }
-
-        /// <summary>
-        /// Retrieve the extended <see cref="IEnumerable{UserRoleDao}"/> implementation as <see cref="IEnumerable{Claim}"/>
-        /// </summary>
-        /// <param name="userRoles">The extended <see cref="IEnumerable{UserRoleDao}"/> to convert</param>
-        /// <returns>The converted <see cref="IEnumerable{Claim}"/> implementation</returns>
-        public static IEnumerable<Claim> ToRoleClaims(this IEnumerable<UserRoleDao> userRoles)
-        {
-            return userRoles.IsNotNullNorEmpty() ? userRoles.SelectMany(ToRoleClaims) : default;
         }
 
         /// <summary>
